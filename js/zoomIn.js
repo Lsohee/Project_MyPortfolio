fe3cd51cfdb98ee62eb795e3bad5a9c05ab9a5ef
limsohee <limsohee0521@gmail.com>
2022-05-31 15:17:34 +0900

    원인 찾음 해결

--- js/zoomIn.js
+++ js/zoomIn.js
@@ -14,7 +14,7 @@ console.log(currentHeight)
 /* 
 내가 넣은 수 / currentWidth * 100 = 내가 원하는 수
 
- */ 
+ */
 
 
 // console.log(100/currentWidth)
@@ -29,108 +29,95 @@ window.addEventListener("wheel", function () {
   let docTotalHeight = this.document.body.offsetHeight;
   if (scrollHeight + windowHeight > docTotalHeight) {
     root1.remove();
-    start()
+    // start()
   }
 })
 
 
 
-// todo 이제 적용될 값을 구하는 함수 or 현재 값을 구하는 함수
-// todo 뭐가 맞을까? 적용되는 값을 고정 시키고 현재 값을 넣는게..?
 
-/*
-줌인 
-layerDiv.animate([
-{transform : 바로 전 단계 값},
-{transform : 현재 적용될 값}
-])
 
-A = 바로 전 단계 값
-changeValue = 현재 적용될 값
-layerDiv.animate([
-{transform : A },
-{transform : A와 changeValue를 조합}
-...
-])
-A와 changeValue를 조합한 값을 A로 재할당?
 
-memo 나의 설계(예상)
-A = 바로 전 단계 값
-changeValue = 현재 적용될 값
-B = A 와 changeValue를 조합한 값
-layerDiv.animate([
-{transform : A },
-{transform : B }
-...
-])
-A = B
-*/
 
-/*
-원하는 방향
-무엇을 각각의 layerDiv을 
-어떻게 전후 비율을 일정하게 변화를 주는 기계 (함수)
-변수는? layerDiv, 변화하는 비율, 현재 값, 적용될 값
-현재 값에 변화하는 비율을 연산 = 적용될 값
-지금 적용될 값이 다음의 현재값이 됨
 
-*/
 
-let zoomIn = function (layerDiv, scale, x, y, ratio, changeX, changeY){
+
+
+
+function start(layerDiv, scale, x, y, ratio, changeX, changeY, handler1, handler2, handler3, callback) {
+  console.log(layerDiv.outerText);
+  callback(layerDiv, scale, x, y, ratio, changeX, changeY, handler1, handler2, handler3);
+}
+
+
+
+
+
+
+
+
+
+
+let zoomIn = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
   layerDiv.animate([{
-  transform: `scale(${scale}) 
+      transform: `scale(${scale}) 
   translate(${x/currentWidth*100}%, ${y/currentHeight*100}%)`
-},
-  {
-    transform: `scale(${scale +ratio}) 
+    },
+    {
+      transform: `scale(${scale +ratio}) 
     translate(${(x+changeX)/currentWidth*100}%,${(y+changeY)/currentHeight*100}%)`
-  }
-], {
-  duration: 2000,
-  fill: "forwards"
-})
+    }
+  ], {
+    duration: 2000,
+    fill: "forwards"
+  })
 }
 
 let zoomOut = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
-layerDiv.animate([{
-  transform: `scale(${scale+ratio}) 
+  layerDiv.animate([{
+      transform: `scale(${scale+ratio}) 
   translate(${(x+changeX)/currentWidth*100}%, ${(y+changeY)/currentHeight*100}%)`
-},
-{
-  transform: `scale(${scale}) 
+    },
+    {
+      transform: `scale(${scale}) 
   translate(${(x)/currentWidth*100}%,${(y)/currentHeight*100}%)`
-  }
-], {
-  duration: 2000,
-  fill: "forwards"
-})
+    }
+  ], {
+    duration: 2000,
+    fill: "forwards"
+  })
 }
 
-let currentValue = "a" 
+// let currentValue = "a"
 
-function zoom(layerDiv, scale, x, y, ratio, changeX, changeY,handler1,handler2,handler3) {
-  
+
+
+function zoom(layerDiv, scale, x, y, ratio, changeX, changeY, handler1, handler2, handler3) {
   root2.addEventListener("wheel", (event) => {
+currentValue = handler1
+
     if (event.wheelDelta < 0 && currentValue === handler1) { //줌인 두번째 -> 결과 : I 페이지
+      console.log(currentValue)
       console.log("go to page Next")
-      currentValue = handler2
       zoomIn(layerDiv, scale, x, y, ratio, changeX, changeY)
-      
+      currentValue = handler2
     } else if (event.wheelDelta > 0 && currentValue === handler1) { //줌인 두번째 -> 결과 : My 페이지
-      // 재할당한 현재위치에 다시 줌인 효과 넣기
       console.log("go to page last");
       zoomOut(layerDiv, scale, x, y, ratio, changeX, changeY)
       currentValue = handler3
-      // 줌인 효과 넣은 위치를 다시 현재 위치에 재할당
     }
   })
-  
+
 }
 
+start(layerArr[0], 1, 0, 0, 2, 2, 1, "a", "b", "c", zoom);
+start(layerArr[1], 1, 0, 0, 0.5, 2, 1, "a", "b", "c", zoom);
+start(layerArr[2], 1, 0, 0, 2, 2, 1, "a", "b", "c", zoom);
+start(layerArr[3], 1, 0, 0, 2, 2, 1, "a", "b", "c", zoom);
+// start는 비동기 방식으로 작동함
+//callback함수가 작동을 안함
 
 
-  zoom(layerArr[0],1,0,0,2,2,1,"a","b","c")
-  // 이거 작동이 안끝나서 다음 함수가 콜링이 안됨
-  zoom(layerArr[1],1,0,0,2,2,1,"a","b","c")   
-  zoom(layerArr[2],1,0,0,2,2,1,"a","b","c")
-// 같은 함수를 동시 다발적으로 호출하는 방법은? --> 생성자 함수?
+// ! 이벤트의 스위치를 달기 위해 핸들러 변수를 재할당 해준게 다른 호출된 함수에 영향을 미쳤음
+// memo 실제로  핸들러 변수를 이벤트 안에서 재할당 해주니까 정상 작동함
+ 
