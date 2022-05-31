9e9ac74079fc2a30444e013b986c0b28e3ea02b9
limsohee <limsohee0521@gmail.com>
2022-05-31 11:15:08 +0900

    함수화 성공 -> 비동시 처리가 필요함 (연구 필요)

--- js/zoomIn.js
+++ js/zoomIn.js
@@ -8,12 +8,19 @@ console.dir(root2);
 
 let currentWidth = root2.offsetWidth;
 let currentHeight = root2.offsetHeight;
-
+console.log(currentHeight)
 
 // todo 값을 퍼센트로 변환하는 식 만들기
-let widthPercent = Math.round(100 /currentWidth);
-let heightPercent = Math.round(100/ currentHeight );
+/* 
+내가 넣은 수 / currentWidth * 100 = 내가 원하는 수
 
+ */ 
+
+
+// console.log(100/currentWidth)
+// let widthPercent = Math.round(100 currentWidth);
+// let heightPercent = Math.round(100/ currentHeight );
+// console.log(widthPercent)
 
 // memo 스크롤 다시 올릴때 root1 안나오게 없앰
 window.addEventListener("wheel", function () {
@@ -22,7 +29,7 @@ window.addEventListener("wheel", function () {
   let docTotalHeight = this.document.body.offsetHeight;
   if (scrollHeight + windowHeight > docTotalHeight) {
     root1.remove();
-    start(layerArr[0])
+    start()
   }
 })
 
@@ -69,88 +76,61 @@ A = B
 
 */
 
-
-
-
-
-let zoom = {
-  zoomIn: function (layerDiv, scale, x, y, ratio, changeX, changeY) {
-    layerDiv.animate([{
-      transform: `scale(${scale}) 
-      translate(${x*widthPercent}%, ${y*heightPercent}%)`
-    },
-      {
-        transform: `scale(${scale +ratio}) 
-        translate(${(x+changeX*20)*widthPercent}%,${(y+changeY*20)*heightPercent}%)`
-      }
-    ], {
-      duration: 2000,
-      fill: "forwards"
-    })
-  },
-  
-  zoomOut: function (layerDiv, scale, x, y, ratio, changeX, changeY) {
-    layerDiv.animate([{
-      transform: `scale(${scale}) 
-      translate(${x}%, ${y}%)`
-    },
-    {
-      transform: `scale(${scale -ratio}) 
-      translate(${(x-changeX)*widthPercent}%,${(y-changeY)*heightPercent}%)`
-      }
-    ], {
-      duration: 2000,
-      fill: "forwards"
-    })
+let zoomIn = function (layerDiv, scale, x, y, ratio, changeX, changeY){
+  layerDiv.animate([{
+  transform: `scale(${scale}) 
+  translate(${x/currentWidth*100}%, ${y/currentHeight*100}%)`
+},
+  {
+    transform: `scale(${scale +ratio}) 
+    translate(${(x+changeX)/currentWidth*100}%,${(y+changeY)/currentHeight*100}%)`
   }
+], {
+  duration: 2000,
+  fill: "forwards"
+})
 }
 
+let zoomOut = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
+layerDiv.animate([{
+  transform: `scale(${scale+ratio}) 
+  translate(${(x+changeX)/currentWidth*100}%, ${(y+changeY)/currentHeight*100}%)`
+},
+{
+  transform: `scale(${scale}) 
+  translate(${(x)/currentWidth*100}%,${(y)/currentHeight*100}%)`
+  }
+], {
+  duration: 2000,
+  fill: "forwards"
+})
+}
 
+let currentValue = "a" 
 
-
-
-
-
-// memo 스위치 역할을 할 핸들러 변수 생성
-currentValue = "a";
-
-let start = function () {
+function zoom(layerDiv, scale, x, y, ratio, changeX, changeY,handler1,handler2,handler3) {
+  
   root2.addEventListener("wheel", (event) => {
-    if (event.wheelDelta < 0 && currentValue === 'a') { //줌인 두번째 -> 결과 : I 페이지
-
-      zoom.zoomIn(layerArr[0],1,layerArr[0].offsetWidth,layerArr[0].offsetHeight,2,-50,12),
-      zoom.zoomIn(layerArr[1],1,layerArr[1].offsetWidth,layerArr[1].offsetHeight,0.7,2,2),
-      zoom.zoomIn(layerArr[2],1,layerArr[2].offsetWidth,layerArr[2].offsetHeight,0.6,2,2),
-      zoom.zoomIn(layerArr[3],1,layerArr[3].offsetWidth,layerArr[3].offsetHeight,0.2,2,2),
+    if (event.wheelDelta < 0 && currentValue === handler1) { //줌인 두번째 -> 결과 : I 페이지
+      console.log("go to page Next")
+      currentValue = handler2
+      zoomIn(layerDiv, scale, x, y, ratio, changeX, changeY)
       
-      
-      console.log("go to page I ")
-      currentValue = "b"
-      //줌인 효과 넣은 수를 다시 현재 위치로 재할당
-    } else if (event.wheelDelta < 0 && currentValue === "b") { //줌인 두번째 -> 결과 : My 페이지
+    } else if (event.wheelDelta > 0 && currentValue === handler1) { //줌인 두번째 -> 결과 : My 페이지
       // 재할당한 현재위치에 다시 줌인 효과 넣기
-      console.log("go to page My");
-      currentValue = "c"
+      console.log("go to page last");
+      zoomOut(layerDiv, scale, x, y, ratio, changeX, changeY)
+      currentValue = handler3
       // 줌인 효과 넣은 위치를 다시 현재 위치에 재할당
-    } else if (event.wheelDelta < 0 && currentValue === "c") { //줌인 세번째 -> 결과 : Me 페이지
-      console.log("go to page Me");
-      currentValue = "d"
-    } else if (event.wheelDelta < 0 && currentValue === "d") { //줌인 네번째 -> 결과 : Myself 페이지
-      console.log("go to page Myself");
-    } else if (event.wheelDelta > 0 && currentValue === "a") { // 줌아웃 -> 0페이지
-      console.log("back to page 0 ")
-      // ? 여기 작동 안되는 듯
-    } else if (event.wheelDelta > 0 && currentValue === "b") { // 줌아웃 두번째-> I 페이지
-      console.log("back to page I ");
-      currentValue = "a"
-    } else if (event.wheelDelta > 0 && currentValue === "c") { // 줌아웃 세번째 -> My페이지
-      console.log("back to page My");
-      currentValue = "b"
-      // currentValue = "c"
-    } else if (event.wheelDelta > 0 && currentValue === "d") { // 줌아웃  네번째 -> Me페이지
-      console.log("back to page Me");
-      currentValue = "c"
     }
-
   })
+  
 }
+
+
+
+  zoom(layerArr[0],1,0,0,2,2,1,"a","b","c")
+  // 이거 작동이 안끝나서 다음 함수가 콜링이 안됨
+  zoom(layerArr[1],1,0,0,2,2,1,"a","b","c")   
+  zoom(layerArr[2],1,0,0,2,2,1,"a","b","c")
+// 같은 함수를 동시 다발적으로 호출하는 방법은? --> 생성자 함수?
