a93a8f355a9c1aa03050170478ad317f5fc9d701
limsohee <limsohee0521@gmail.com>
2022-06-08 17:35:55 +0900

    스위치 틀을 만들었으니 구체적인 값을 넣어서 움직임 조정할 것

--- js/zoomIn220608.js
+++ js/zoomIn220608.js
@@ -5,6 +5,7 @@ const layerArr = Array.from(root2.children);
 
 
 
+let currentPage = [true, false, false, false, false]
 
 
 // memo 스크롤 다시 올릴때 root1 안나오게 없앰
@@ -23,94 +24,111 @@ window.addEventListener("wheel", function () {
   }
 })
 
-  function movement() {
-
-    
-    
-}
 
 
 
 
-function start(layerDiv, scale, x, y, ratio, changeX, changeY, currentPageIndex, callback) {
-  callback(layerDiv, scale, x, y, ratio, changeX, changeY, currentPageIndex);
-}
-
-
-let zoomIn = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
-  layerDiv.animate([{
-      transform: `scale(${scale}) 
+let zoom = [
+  zoomIn = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
+    layerDiv.animate([{
+        transform: `scale(${scale}) 
   translate(${x/currentWidth*100}%, ${y/currentHeight*100}%)`
-    },
-    {
-      transform: `scale(${scale +ratio}) 
+      },
+      {
+        transform: `scale(${scale +ratio}) 
     translate(${(x+changeX)/currentWidth*100}%,${(y+changeY)/currentHeight*100}%)`
-    }
-  ], {
-    duration: 2000,
-    fill: "forwards"
-  })
-}
-
-let zoomOut = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
-  layerDiv.animate([{
-      transform: `scale(${scale+ratio}) 
-  translate(${(x+changeX)/currentWidth*100}%, ${(y+changeY)/currentHeight*100}%)`
-    },
-    {
-      transform: `scale(${scale}) 
-  translate(${(x)/currentWidth*100}%,${(y)/currentHeight*100}%)`
-    }
-  ], {
-    duration: 2000,
-    fill: "forwards"
-  })
-}
-
-function zoom(currentPageIndex) {
-  root2.addEventListener("wheel", (event) => {
-    if (event.wheelDelta < 0 && currentPage[currentPageIndex] === true) {
-    } 
-    else if (event.wheelDelta > 0 &&currentPage[currentPageIndex] === true) {
       }
-    zeroToI()
-    IToMy()
-      })
-  
-  let currentPage = [true, false, false, false, false]
-  console.log("go to page Next")
-  zoomIn(layerDiv, scale, x, y, ratio, changeX, changeY) // ^ 1
-  currentPage.splice(currentPageIndex, 2, false, true) // ^ 2 
-  console.log(currentPage)
-  console.log("go to page last");
-  zoomOut(layerDiv, scale, x, y, ratio, changeX, changeY) // ^ 4
-  currentPage.splice(currentPageIndex, 2, true, false) // ^ 3
-  console.log(currentPageIndex)
+    ], {
+      duration: 2000,
+      fill: "forwards"
+    })
+  },
+  zoomOut = function (layerDiv, scale, x, y, ratio, changeX, changeY) {
+    layerDiv.animate([{
+        transform: `scale(${scale+ratio}) 
+  translate(${(x+changeX)/currentWidth*100}%, ${(y+changeY)/currentHeight*100}%)`
+      },
+      {
+        transform: `scale(${scale}) 
+  translate(${(x)/currentWidth*100}%,${(y)/currentHeight*100}%)`
+      }
+    ], {
+      duration: 2000,
+      fill: "forwards"
+    })
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  // ? 코드를 조금더 짧고 간단하게 만들 수 는 없을까?
+
+]
 
 
+
+
+
+
+
+
+// memo 줌인 줌아웃 제어 함수
+function movement() {
+  root2.addEventListener("wheel", (event) => {
+    if (event.wheelDelta > 0 && currentPage[0] === true) {
+      console.log("움직이지 않습니다")
+
+    } else if (event.wheelDelta < 0 && currentPage[0] === true) {
+      console.log("zero에서 I로 이동합니다")
+      currentPage.splice(0, 2, false, true)
+      console.log(currentPage)
+      zeroToI()
+    } else if (event.wheelDelta < 0 && currentPage[1] === true) {
+      console.log("I에서 My로 이동합니다")
+      currentPage.splice(1, 2, false, true)
+      console.log(currentPage)
+
+    } else if (event.wheelDelta < 0 && currentPage[2] === true) {
+      console.log("My에서 Me로 이동합니다")
+      currentPage.splice(2, 2, false, true)
+      console.log(currentPage)
+
+    } else if (event.wheelDelta < 0 && currentPage[3] === true) {
+      console.log("Me에서 Myself로 이동합니다")
+      currentPage.splice(3, 2, false, true)
+      console.log(currentPage)
+
+    } else if (event.wheelDelta > 0 && currentPage[4] === true) {
+      console.log("Myself에서 Me로 이동합니다")
+      currentPage.splice(3, 2, true, false)
+      console.log(currentPage)
+    } else if (event.wheelDelta > 0 && currentPage[3] === true) {
+      console.log("Me에서 My로 이동합니다")
+      currentPage.splice(2, 2, true, false)
+      console.log(currentPage)
+    } else if (event.wheelDelta > 0 && currentPage[2] === true) {
+      console.log("My에서 I로 이동합니다")
+      currentPage.splice(1, 2, true, false)
+      console.log(currentPage)
+    } else if (event.wheelDelta > 0 && currentPage[1] === true) {
+      console.log("I에서 zero로 이동합니다")
+      currentPage.splice(0, 2, true, false)
+      console.log(currentPage)
+    }
+  })
+
+}
+
+
+
+
+// memo 움직임 제어를 위한 함수 호출을 레이어와 움직임 종류로 묶는 용도 함수
 // memo zero - I 1번이 실행되면 
 function zeroToI() {
-  start(layerArr[0], 1, 0, 0, 2, -50, 1, 0,  zoom);
-  start(layerArr[1], 1, 0, 0, 0.5, -60, 1,  0, zoom);
-  start(layerArr[2], 1, 0, 0, 0.2, -20, 1,  0, zoom);
-  start(layerArr[3], 1, 0, 0, 0, 0, 1,  0,  zoom);
+  //  ? 배열과 객체를 어떻게 이용하면 숫자를 한번만쓸 수 있을 거 같은데
+  zoom[0](layerArr[0], 1, 0, 0, 2, -50, 1);
+  zoom[0](layerArr[1], 1, 0, 0, 0.5, -60, 1);
+  zoom[0](layerArr[2], 1, 0, 0, 0.2, -20, 1);
+  zoom[0](layerArr[3], 1, 0, 0, 0, 0, 1);
 }
 // memo I - My 2번이 실행되게
 function IToMy() {
-  start(layerArr[0], 1, 0, 0, 3, -50, 1, 1,  zoom);
-  start(layerArr[1], 1, 0, 0, 0.5, -60, 1,  1, zoom);
-  start(layerArr[2], 1, 0, 0, 0.2, -20, 1,  1, zoom);
-  start(layerArr[3], 1, 0, 0, 0, 0, 1,  1,  zoom);
+
 }
 // memo My - Me 3번이 실행되게
 function MyToMe() {
@@ -120,49 +138,3 @@ function MyToMe() {
 function MyToMe() {
 
 }
-
-
-
-
-// root2.addEventListener("wheel", (event) => {
-//   if (event.wheelDelta > 0 && currentPage[0] === true) {
-//     console.log("움직이지 않습니다")
-
-//   } else if (event.wheelDelta < 0 && currentPage[0] === true) {
-//     console.log("zero에서 I로 이동합니다")
-//     currentPage.splice(0, 2, false, true)
-//     console.log(currentPage)
-
-//   } else if (event.wheelDelta < 0 && currentPage[1] === true) {
-//     console.log("I에서 My로 이동합니다")
-//     currentPage.splice(1, 2, false, true)
-//     console.log(currentPage)
-
-//   } else if (event.wheelDelta < 0 && currentPage[2] === true) {
-//     console.log("My에서 Me로 이동합니다")
-//     currentPage.splice(2, 2, false, true)
-//     console.log(currentPage)
-
-//   } else if (event.wheelDelta < 0 && currentPage[3] === true) {
-//     console.log("Me에서 Myself로 이동합니다")
-//     currentPage.splice(3, 2, false, true)
-//     console.log(currentPage)
-
-//   } else if (event.wheelDelta > 0 && currentPage[4] === true) {
-//     console.log("Myself에서 Me로 이동합니다")
-//     currentPage.splice(3, 2, true, false)
-//     console.log(currentPage)
-//   } else if (event.wheelDelta > 0 && currentPage[3] === true) {
-//     console.log("Me에서 My로 이동합니다")
-//     currentPage.splice(2, 2, true, false)
-//     console.log(currentPage)
-//   } else if (event.wheelDelta > 0 && currentPage[2] === true) {
-//     console.log("My에서 I로 이동합니다")
-//     currentPage.splice(1, 2, true, false)
-//     console.log(currentPage)
-//   } else if (event.wheelDelta > 0 && currentPage[1] === true) {
-//     console.log("I에서 zero로 이동합니다")
-//     currentPage.splice(0, 2, true, false)
-//     console.log(currentPage)
-//   }
-// })
