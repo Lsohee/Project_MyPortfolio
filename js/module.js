664305e3feb62b1b5849828a4c831b06b5794ff7
limsohee <limsohee0521@gmail.com>
2022-06-21 11:34:32 +0900

    아무래도 module화 다시 공부하고 리팩토링 시도해야겠음 일단 보류

--- js/module.js
+++ js/module.js
@@ -2,24 +2,10 @@ const container = document.getElementById("container");
 const root1 = document.getElementById("root1")
 const root2 = document.getElementById("root2");
 const layerArr = Array.from(root2.children);
-
-
-
 let zoomArr, startSplice
-
-
 let currentWidth = root2.offsetWidth;
 let currentHeight = root2.offsetHeight;
 
-
-
-
-import module from 'scroll';
-import {zoom} from "movingZoom.js"
-import {waterColorDeep} from "waterBackgroundFilter.js"
-import { currentPage, movement } from "./zoomSwitch"
-import { zeroToI } from "./layerMovement/zeroToI"
-import { MyToMe } from './layerMovement/MyToMe';
-import { MeToMyself } from './layerMovement/MeToMyself';
-import { MyselfToLast } from './layerMovement/MyselfToLast';
-import { LastToMyself } from './layerMovement/LastToMyself';
+console.log("module")
+// module은 읽히는 것 확인
+// import zoom from './movingZoom.js';
--- js/movingZoom.js
+++ js/movingZoom.js
@@ -32,4 +32,4 @@ let zoom = [
 
 ]
 
-export{zoom}
+export default zoom
