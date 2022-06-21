bc32a3e91fa394d3a1c4d3a63203da1e2782bf17
limsohee <limsohee0521@gmail.com>
2022-06-21 17:25:27 +0900

    모듈화중

---
+++ repack/module.js
@@ -0,0 +1,6 @@
+const container = document.getElementById("container");
+const roo1 = document.getElementById("root1");
+const root2 = document.getElementById("root2");
+
+console.log("module")
+import CreateLayers from "./createLayers.js";
