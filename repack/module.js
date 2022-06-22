0be34186bf4d22190b2a41084aa4f0faef26847d
limsohee <limsohee0521@gmail.com>
2022-06-22 11:55:32 +0900

    innerHTML을 활용해서 동적으로 요소를 생성함

--- repack/module.js
+++ repack/module.js
@@ -1,6 +1,16 @@
-const container = document.getElementById("container");
+// const container = document.getElementById("container");
 const roo1 = document.getElementById("root1");
-const root2 = document.getElementById("root2");
+// const root2 = document.getElementById("root2");
 
 console.log("module")
-import CreateLayers from "./createLayers.js";
+import {createLayer1, createLayer2} from "./createLayers.js";
+
+// function layers(){
+// return
+// }
+
+let layer = [createLayer1(),createLayer2()]
+
+
+
+root1.innerHTML =  layer[0]
